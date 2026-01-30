7f2d6d1496c6b8843c36370c94bfb759f472a20b
Venu803 <venugopalp23cys@psnacet.edu.in>
2026-01-30 23:26:56 +0530

    Refactor Hangman game code formatting

--- CodeAlpha_Hangman/Hangman.py
+++ CodeAlpha_Hangman/Hangman.py
@@ -4,18 +4,17 @@ word = random.choice(words)
 guessed_letters = []
 attempts = 6
 display_word = ["-"] * len(word)
-print(" Welcome to Hangman Game!")
+print("Welcome to Hangman Game!")
 print("Guess the word, one letter at a time")
 while attempts > 0 and "-" in display_word:
     print("\nWord:", " ".join(display_word))
     print("Attempts Left:", attempts)
+
     guess = input("Enter a letter: ").lower()
     if guess in guessed_letters:
-        print(" You already guessed that letter!")
+        print("You already guessed that letter!")
         continue
-
     guessed_letters.append(guess)
-
     if guess in word:
         print("Correct guess!")
         for i in range(len(word)):
